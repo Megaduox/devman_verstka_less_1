233ebf41d7ca24f78cf815f7371baa10c882ffe5
Megaduox <72254723+Megaduox@users.noreply.github.com>
2021-04-08 14:11:13 +0300

    Updated requirements.tst, renamed variable
    Renamed variable production
    Added new version of pandas library

--- main.py
+++ main.py
@@ -36,10 +36,10 @@ def main():
     args = parser.parse_args()
     excel_file = pd.read_excel(args.file_name, sheet_name="Лист1", na_values=False, keep_default_na=False)
     records_from_excel_file = excel_file.to_dict('records')
-    pruduction = defaultdict(list)
+    production = defaultdict(list)
 
     for record in records_from_excel_file:
-        pruduction[record['Категория']].append(record)
+        production[record['Категория']].append(record)
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -50,7 +50,7 @@ def main():
 
     rendered_page = template.render(
         years_with_you=years(),
-        items=pruduction
+        items=production
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
